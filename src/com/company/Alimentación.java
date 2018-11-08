23208975f137930a20b68d2f9c759cca7f1ca8fa
Christian <cruizm97@elpuig.xeill.net>
2018-11-08 16:57:54 +0100

    Initial commit

---
+++ src/com/company/Alimentación.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Alimentación {
+
+    Calendar diaactual = new GregorianCalendar();
+    int año =diaactual.get(Calendar.YEAR);
+    int mes =diaactual.get(Calendar.MONTH);
+    int dia =diaactual.get(Calendar.DAY_OF_MONTH);
+
+
+}
---
+++ src/com/company/Electronica.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Electronica {
+
+    int preu;
+    int diesGarantia;
+}
